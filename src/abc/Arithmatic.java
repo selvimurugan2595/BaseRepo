bf724f3801f9fc985512d3fda9e6efe4d436bf4e
Yugandhar786 <vgyugandhargopi2708@gmail.com>
2024-07-21 21:10:38 +0530

    Arithmatic Function Code added

---
+++ src/abc/Arithmatic.java
@@ -0,0 +1,18 @@
+package abc;
+
+public class Arithmatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c=30;
+		int d,e,f;
+		d=a+b;
+		e=c*a;
+		f=b/a;
+		System.out.println("addition of a and b "+d);
+		System.out.println("multiplication of c and a " +e);
+		System.out.println("Division of b and b "+f);
+
+	}
+
+}
