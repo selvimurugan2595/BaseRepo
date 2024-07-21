044d19e1e01b5bdebb45dd0984158af584ee1fd3
Yugandhar786 <vgyugandhargopi2708@gmail.com>
2024-07-21 21:29:29 +0530

    Simple login Logic

---
+++ src/abc/Loops.java
@@ -0,0 +1,35 @@
+package abc;
+import java.util.Scanner;
+public class Loops{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner obj1 = new Scanner(System.in);  
+	    System.out.println("Enter username");
+
+	    String userName = obj1.nextLine(); 
+	    
+	    
+	    Scanner obj2 = new Scanner(System.in);  
+	    System.out.println("Enter Password");
+	    String passWord = obj2.nextLine(); 
+	    System.out.println(userName);
+	    System.out.println(passWord);
+	    
+	    if(userName.equals("admin") && passWord.equals("Yuga"))
+	    {
+	    	System.out.println("Login Sucessfull");
+	    	
+	    }
+	    else
+	    {
+	    	System.out.println("Login Failed");
+	    }
+	
+		
+		
+
+	}
+
+}
